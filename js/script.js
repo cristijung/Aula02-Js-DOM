7122b43c64da5525e70031db16a59c72bd168d65
Cristina Jung <cristijung@gmail.com>
2023-07-26 15:42:09 -0300

    imagem + filtro

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ function changeColor(newColor) {
     elem.style.color = newColor;
 }
 
+//-------------------------------------------------
+
+function trocarFiltro(type) {
+    let image = document.getElementById('baleia');
+
+    if (type == 'gray') {
+        image.style.filter = 'grayscale(100%)';
+    } else if (type == 'sepia') {
+        image.style.filter = 'sepia(100%)';
+    } else {
+        image.style.filter = 'none'
+    }
+}
